196f5b2db612625f8856fe13143532562d0906a0
Hayder <hayder9701@gmail.com>
2021-09-17 19:30:53 -0400

    Add Stardog sensor repository tests

---
+++ src/test/java/org/traffic/traffic_registry/sensor/StardogRDF4JSensorRepositoryTest.java
@@ -0,0 +1,53 @@
+package org.traffic.traffic_registry.sensor;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.StringReader;
+
+@Slf4j
+@ExtendWith(VertxExtension.class)
+class StardogRDF4JSensorRepositoryTest {
+
+  private static JsonObject config;
+
+  private static Sensor sensor;
+
+  private static String rdfSensor;
+
+  @BeforeAll
+  static void configure(Vertx vertx) {
+    config = new JsonObject(vertx.fileSystem().readFileBlocking("sensors/sensor-repository.json"));
+    sensor =
+        Sensor.fromJson(new JsonObject(vertx.fileSystem().readFileBlocking("sensors/sensor.json")));
+    rdfSensor = vertx.fileSystem().readFileBlocking("sensors/sensor.ttl").toString();
+  }
+
+  @Test
+  @DisplayName("When a sensor is saved then the return RDF format must match")
+  void saveTest(VertxTestContext testContext) {
+    val repository = new StardogRDF4JSensorRepository(config);
+    val result = repository.save(sensor);
+    result.onComplete(
+        testContext.succeeding(
+            rdf ->
+                testContext.verify(
+                    () -> {
+                      val given = Rio.parse(new StringReader(rdf), RDFFormat.TURTLE);
+                      val expected = Rio.parse(new StringReader(rdfSensor), RDFFormat.TURTLE);
+                      Assertions.assertEquals(expected, given);
+                      testContext.completeNow();
+                    })));
+  }
+}
