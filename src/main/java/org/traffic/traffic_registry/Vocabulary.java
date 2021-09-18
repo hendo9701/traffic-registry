959da5775c992a56116cc137340b8c06e50039f1
Hayder <hayder9701@gmail.com>
2021-09-18 01:28:41 -0400

    Extend Vocabulary

--- src/main/java/org/traffic/traffic_registry/Vocabulary.java
+++ src/main/java/org/traffic/traffic_registry/Vocabulary.java
@@ -19,5 +19,29 @@ public final class Vocabulary {
 
   public static final IRI HAS_QUANTITY_KIND = Values.iri(IOT_LITE, "hasQuantityKind");
 
+  public static final Namespace GEO =
+      Values.namespace("wgs84_pos", "https://www.w3.org/2003/01/geo/wgs84_pos/");
+
+  public static final IRI LOCATION = Values.iri(GEO, "location");
+
+  public static final IRI POINT = Values.iri(GEO, "Point");
+
+  public static final IRI LATITUDE = Values.iri(GEO, "lat");
+
+  public static final IRI LONGITUDE = Values.iri(GEO, "long");
+
+  public static final Namespace IOT_STREAM =
+      Values.namespace("iot-stream", "http://purl.org/iot/ontology/iot-stream/");
+
+  public static final IRI STREAM_START = Values.iri(IOT_STREAM, "windowStart");
+
+  public static final IRI STREAM_END = Values.iri(IOT_STREAM, "windowEnd");
+
+  public static final IRI DERIVED_FROM = Values.iri(IOT_STREAM, "derivedFrom");
+
+  public static final IRI ANALYSED_BY = Values.iri(IOT_STREAM, "analysedBy");
+
+  public static final IRI GENERATED_BY = Values.iri(IOT_STREAM, "generatedBy");
+
   private Vocabulary() {}
 }
